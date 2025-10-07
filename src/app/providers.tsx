5dac4974bf1159ae6da9d4f6ee5bd98e1a293c74
Pavel Peroutka <pavel.peroutka@rohlik.cz>
2025-10-07 13:45:39 +0200

    Remove nonexisting prop

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,7 @@ import { HeroUIProvider } from "@heroui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider defaultMode="light">
+    <HeroUIProvider>
       {children}
     </HeroUIProvider>
   );
